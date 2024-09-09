2f898e9d8373bcae0e72112fa788909dd322910b
marywebby <marywebby1999@gmail.com>
2024-09-09 11:13:46 -0500

    implement changes

--- portfolio-mcw/src/App.js
+++ portfolio-mcw/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="Portfolio">
+      <header className="Portfolio-header">
+        <h1>Portfolio</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
