9be56fdef4a809b1bfb4243d8a1dce444096ceb9
marywebby <marywebby1999@gmail.com>
2024-09-10 15:53:13 -0500

    padding in navbar fixed

--- portfolio-mcw/src/components/NavBar.js
+++ portfolio-mcw/src/components/NavBar.js
@@ -31,7 +31,7 @@ export const NavBar = ({ navItems, className }) => {
         animate={{ y: visible ? 0 : -100, opacity: visible ? 1 : 0 }}
         transition={{ duration: 0.2 }}
         className={cn(
-          "flex max-w-fit fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-2 items-center justify-center space-x-4",
+            "flex max-w-fit fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-4 pl-6 py-2 items-center justify-center space-x-4",
           className
         )}
       >
@@ -48,7 +48,7 @@ export const NavBar = ({ navItems, className }) => {
           </Link>
         ))}
         <button
-        className="bg-black text-xl font-medium text-white border border-neutral-200 dark:border-white/[0.2] px-6 py-4 rounded-full relative hover:bg-gray-800"
+        className="bg-black text-lg font-medium text-white border border-neutral-200 dark:border-white/[0.2] px-6 py-4 rounded-full relative hover:bg-gray-800"
         >
         <span>About Me</span>
         <span
@@ -56,9 +56,9 @@ export const NavBar = ({ navItems, className }) => {
         />
         </button>
         <button
-        className="bg-black text-xl font-medium text-white border border-neutral-200 dark:border-white/[0.2] px-6 py-4 rounded-full relative hover:bg-gray-800"
+        className="bg-black text-lg font-medium text-white border border-neutral-200 dark:border-white/[0.2] px-6 py-4 rounded-full relative hover:bg-gray-800"
         >
-        <span>About Me</span>
+        <span>Projects</span>
         <span
             className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent h-px"
         />
