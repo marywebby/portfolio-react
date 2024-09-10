e68181a3a2ab6fb729d999a70ce08d8288e4de44
marywebby <marywebby1999@gmail.com>
2024-09-10 15:46:55 -0500

    created nav bar

---
+++ portfolio-mcw/src/components/ui/NavBarMenu.js
@@ -0,0 +1,25 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import { MenuItem } from "./MenuItem"; // Import MenuItem from its file
+
+const transition = {
+  type: "spring",
+  mass: 0.5,
+  damping: 11.5,
+  stiffness: 100,
+  restDelta: 0.001,
+  restSpeed: 0.001,
+};
+
+export const NavBarMenu = ({ setActive, children }) => { // Ensure correct export
+  return (
+    <nav
+      onMouseLeave={() => setActive(null)} // resets the state
+      className="relative rounded-full border border-transparent dark:bg-black dark:border-white/[0.2] bg-white shadow-input flex justify-center space-x-4 px-8 py-6"
+    >
+      {children}
+    </nav>
+  );
+};
+
