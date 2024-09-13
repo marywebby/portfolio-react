d46b073bae392753049d8d789bd307f7c73ce33b
marywebby <marywebby1999@gmail.com>
2024-09-13 14:02:08 -0500

    grid box with tailwind

--- portfolio-mcw/src/App.js
+++ portfolio-mcw/src/App.js
@@ -2,19 +2,23 @@ import React from 'react';
 import StarrySky from './components/StarrySky';
 import NavBar from './components/NavBar';
 import './App.css';
+import './css/tailwind.css';
 
 function App() {
   return (
     <div>
       <StarrySky />
-      <NavBar />
-      <div className='right'>
-
+      <div class="grid grid-rows-3 grid-flow-col gap-4">
+        <div class="row-span-3 ...">
+          <NavBar />
+        </div>
+        <div class="col-span-2 ...">
+          about me
+        </div>
+        <div class="row-span-2 col-span-2 ...">
+          work history
+        </div>
       </div>
-      <div className='left'>
-        
-      </div>
-      
     </div>
   );
 }
