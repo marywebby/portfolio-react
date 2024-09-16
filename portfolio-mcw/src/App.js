1c4149c9a7658a6bfe46791c32a3a747386b2312
marywebby <marywebby1999@gmail.com>
2024-09-16 15:23:44 -0500

    edited class names

--- portfolio-mcw/src/App.js
+++ portfolio-mcw/src/App.js
@@ -8,14 +8,14 @@ function App() {
   return (
     <div>
       <StarrySky />
-      <div class="grid grid-rows-3 grid-flow-col gap-4">
+      <div className="grid grid-rows-3 grid-flow-col gap-4">
         <div class="row-span-3 ...">
           <NavBar />
         </div>
-        <div class="col-span-2 ...">
+        <div className="col-span-2 ...">
           about me
         </div>
-        <div class="row-span-2 col-span-2 ...">
+        <div className="row-span-2 col-span-2 ...">
           work history
         </div>
       </div>
