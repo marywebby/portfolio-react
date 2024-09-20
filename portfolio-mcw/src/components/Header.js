10f5d151b0b89db0c8d08028a0db9a79bc8f954e
marywebby <marywebby1999@gmail.com>
2024-09-20 10:25:59 -0500

    applied customized white tailwind color in header, going to use globally

--- portfolio-mcw/src/components/Header.js
+++ portfolio-mcw/src/components/Header.js
@@ -5,10 +5,10 @@ const Header = () => {
     <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
       <div className="lg:flex lg:justify-between lg:gap-4">
         <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
-          <h1 className='text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl'>
+          <h1 className='text-4xl font-bold tracking-tight text-white-75 sm:text-5xl'>
             Mary Webby
           </h1>
-          <h2 className='mt-3 text-lg font-bold uppercase tracking-tight text-slate-200 sm:text-xl'>
+          <h2 className='mt-3 text-lg font-bold uppercase tracking-tight text-white-75 sm:text-xl'>
             Software Developer
           </h2>
           <p className='mt-4 max-w-xs leading-normal text-gray-400'>
@@ -16,10 +16,10 @@ const Header = () => {
           </p>
         </header>
         <div className='pt-24 lg:w-1/2 lg:py-24'>
-          <div className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">
+          <div className="text-sm font-bold uppercase tracking-widest text-white-75 lg:sr-only">
             about me
           </div>
-          <div className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">
+          <div className="text-sm font-bold uppercase tracking-widest text-white-75 lg:sr-only">
             work history
           </div>
         </div>
