dd473f5cae73d70d319c30c5477bcb70f169ccdb
marywebby <marywebby1999@gmail.com>
2024-09-23 16:43:22 -0500

    started on about me section

--- portfolio-mcw/src/components/Body.js
+++ portfolio-mcw/src/components/Body.js
@@ -19,6 +19,17 @@ const Body = () => {
           <div className="text-sm font-bold uppercase tracking-widest text-white-75 lg:sr-only">
             about me
           </div>
+          <div id='about-me'>
+            <p className='mb-4'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillu
+            </p>
+            <p className='mb-4'>
+
+            </p>
+            <p className='mb-4'>
+
+            </p>
+          </div>
           <div className="text-sm font-bold uppercase tracking-widest text-white-75 lg:sr-only">
             work history
           </div>
