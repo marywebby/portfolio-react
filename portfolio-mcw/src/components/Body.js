10ec5bb3e5f930d9edf47dab41b950bea10f990a
marywebby <marywebby1999@gmail.com>
2024-09-25 15:06:35 -0500

    organized about into new component

--- portfolio-mcw/src/components/Body.js
+++ portfolio-mcw/src/components/Body.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Experience from './Experience';
+import About from './About';
 
 const Body = () => {
   return (
@@ -22,24 +23,7 @@ const Body = () => {
           </div>
         </header>
         <main id='content' className='pt-24 lg:w-1/2 lg:py-24'>
-          <section id='about' className='mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24 text-gray-400 text-xl' aria-label='About me'>
-            <div className='sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0'>
-              <h2 className="text-sm font-bold uppercase tracking-widest text-white-75 lg:sr-only">
-                about me
-              </h2>
-            </div>
-            <div>
-              <p className='mb-4'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillu
-              </p>
-              <p className='mb-4'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillu
-              </p>
-              <p className='mb-4'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillu
-              </p>
-            </div>
-          </section>
+          <About />
           <Experience />
         </main>
       </div>
