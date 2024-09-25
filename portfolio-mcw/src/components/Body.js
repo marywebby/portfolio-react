7dab171c1c4111c2fb2334b94c9e6138b0085826
marywebby <marywebby1999@gmail.com>
2024-09-25 10:48:17 -0500

    fixed formatting issue

--- portfolio-mcw/src/components/Body.js
+++ portfolio-mcw/src/components/Body.js
@@ -5,35 +5,41 @@ const Body = () => {
     <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
       <div className="lg:flex lg:justify-between lg:gap-4">
         <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
-          <h1 className='text-4xl font-bold tracking-tight text-white-75 sm:text-5xl'>
-            Mary Webby
-          </h1>
-          <h2 className='mt-3 text-lg font-bold uppercase tracking-tight text-white-75 sm:text-xl'>
-            Software Developer
-          </h2>
-          <p className='mt-4 max-w-xs leading-normal text-gray-400'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit
-          </p>
+          <div>
+            <h1 className='text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl'>
+              Mary Webby
+            </h1>
+            <h2 className='mt-3 text-lg font-bold uppercase tracking-tight text-white-75 sm:text-xl'>
+              Software Developer
+            </h2>
+            <p className='mt-4 leading-normal text-gray-400'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <nav className='nav hidden lg:block' aria-label='In-page jump links'>
+
+            </nav>
+          </div>
         </header>
-        <div className='pt-24 lg:w-1/2 lg:py-24'>
-          <div className="text-sm font-bold uppercase tracking-widest text-white-75 lg:sr-only">
-            about me
-          </div>
-          <div id='about-me'>
-            <p className='mb-4'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillu
-            </p>
-            <p className='mb-4'>
+        <main id='content' className='pt-24 lg:w-1/2 lg:py-24'>
+          <section id='about' className='mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24' aria-label='About me'>
+            <div className='sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0'>
+              <h2 className="text-sm font-bold uppercase tracking-widest text-white-75 lg:sr-only">
+                about me
+              </h2>
+            </div>
+            <div>
+              <p className='mb-4'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillu
+              </p>
+              <p className='mb-4'>
 
-            </p>
-            <p className='mb-4'>
+              </p>
+              <p className='mb-4'>
 
-            </p>
-          </div>
-          <div className="text-sm font-bold uppercase tracking-widest text-white-75 lg:sr-only">
-            work history
-          </div>
-        </div>
+              </p>
+            </div>
+          </section>
+        </main>
       </div>
     </div>
   );
