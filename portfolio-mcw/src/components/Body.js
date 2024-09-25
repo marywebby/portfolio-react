3d5c5f2aec1a233ecdf446c246e631c5046e6dff
marywebby <marywebby1999@gmail.com>
2024-09-25 12:21:50 -0500

    about me section formatted correctly and displaying

--- portfolio-mcw/src/components/Body.js
+++ portfolio-mcw/src/components/Body.js
@@ -12,8 +12,8 @@ const Body = () => {
             <h2 className='mt-3 text-lg font-bold uppercase tracking-tight text-white-75 sm:text-xl'>
               Software Developer
             </h2>
-            <p className='mt-4 leading-normal text-gray-400'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <p className='mt-4 max-w-sm leading-normal text-gray-400'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
             </p>
             <nav className='nav hidden lg:block' aria-label='In-page jump links'>
 
@@ -21,7 +21,7 @@ const Body = () => {
           </div>
         </header>
         <main id='content' className='pt-24 lg:w-1/2 lg:py-24'>
-          <section id='about' className='mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24' aria-label='About me'>
+          <section id='about' className='mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24 text-gray-400 text-xl' aria-label='About me'>
             <div className='sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0'>
               <h2 className="text-sm font-bold uppercase tracking-widest text-white-75 lg:sr-only">
                 about me
@@ -32,10 +32,10 @@ const Body = () => {
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillu
               </p>
               <p className='mb-4'>
-
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillu
               </p>
               <p className='mb-4'>
-
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillu
               </p>
             </div>
           </section>
