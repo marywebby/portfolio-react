3c7a251a961b94e604705723c4c35b2b65608329
marywebby <marywebby1999@gmail.com>
2024-09-25 13:41:00 -0500

    font added and expearience component created

--- portfolio-mcw/src/components/Body.js
+++ portfolio-mcw/src/components/Body.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Body = () => {
   return (
-    <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
+    <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-noto md:px-12 md:py-20 lg:px-24 lg:py-0">
       <div className="lg:flex lg:justify-between lg:gap-4">
         <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
           <div>
