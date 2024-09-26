c20f07e72ce667c57dcff44372effb67abf439c6
marywebby <marywebby1999@gmail.com>
2024-09-26 15:00:03 -0500

    implement projects into body component

--- portfolio-mcw/src/components/Body.js
+++ portfolio-mcw/src/components/Body.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Experience from './Experience';
 import About from './About';
+import Projects from './Projects';
 
 const Body = () => {
   return (
@@ -11,6 +12,7 @@ const Body = () => {
         <main id='content' className='pt-24 lg:w-1/2 lg:py-24'>
           <About />
           <Experience />
+          <Projects />
         </main>
       </div>
     </div>
