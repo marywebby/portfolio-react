b2ae4c5a5aa880e0ccd24305201f99fee416666c
marywebby <marywebby1999@gmail.com>
2024-10-09 16:49:23 -0500

    notes for projects

--- portfolio-mcw/src/components/Projects.js
+++ portfolio-mcw/src/components/Projects.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Projects = () => {
   return (
     <section id='projects' className='mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24 text-gray-400 text-xl' aria-label='projects'>
-        
+        {/* implement projects with links to github and deployed apps if applicable */}
     </section>
   );
 };
