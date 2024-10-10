6f41d8f3d5e643af9fe85b79886941c11a4b6ef7
marywebby <marywebby1999@gmail.com>
2024-10-10 11:30:52 -0500

    implement project unordered list

--- portfolio-mcw/src/components/Projects.js
+++ portfolio-mcw/src/components/Projects.js
@@ -3,7 +3,24 @@ import React from 'react';
 const Projects = () => {
   return (
     <section id='projects' className='mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24 text-gray-400 text-xl' aria-label='projects'>
-        {/* implement projects with links to github and deployed apps if applicable */}
+      <div className='sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0'>
+        <h2 className='text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only'>Projects</h2>
+      </div>
+      <div>
+        <ul className='group/list'>
+          <li className='mb-12'>
+            <div className='group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50'>
+              <div className='absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg'>
+              </div>
+              <div className='z-10 sm:order-2 sm:col-span-6'>
+                <h3>
+                  <a className='inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base' href='https://github.com/marywebby/lineup' target='_blank' rel='noreferrer noopener' aria-aria-label='Implement your own skincare products (opens in a new tab)'></a>
+                </h3>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
     </section>
   );
 };
